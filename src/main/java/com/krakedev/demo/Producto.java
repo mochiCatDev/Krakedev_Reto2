94661582133768a3ed43d00a228d7fe9c6c12ea7
mochiCatDev <230013094+mochiCatDev@users.noreply.github.com>
2026-07-29 17:03:40 -0500

    feat: crear clase Producto con los siguientes atributos codigo, nombre, descripcion y peso.

---
+++ src/main/java/com/krakedev/demo/Producto.java
@@ -0,0 +1,9 @@
+package com.krakedev.demo;
+
+public class Producto() {
+    // atributos
+    private int codigo;
+    private String nombre;
+    private String descripcion;
+    private double peso;
+}
