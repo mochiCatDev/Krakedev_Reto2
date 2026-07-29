107d7d31da09b900cb01449a72b86ace909fec0a
mochiCatDev <230013094+mochiCatDev@users.noreply.github.com>
2026-07-29 17:09:07 -0500

    fix: cambiar identación space con tabs.

--- src/main/java/com/krakedev/demo/Producto.java
+++ src/main/java/com/krakedev/demo/Producto.java
@@ -1,26 +1,26 @@
 package com.krakedev.demo;
 
 public class Producto {
-    private int codigo;
-    private String nombre;
-    private String descripcion;
-    private double peso;
+    private int codigo;
+    private String nombre;
+    private String descripcion;
+    private double peso;
 
-    // constructores
-    public Producto(int codigo, String nombre) {
-        this.codigo = codigo;
-        this.nombre = nombre;
-    }
+    // constructores
+    public Producto(int codigo, String nombre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+    }
 
-    // getters
-    public int getCodigo() { return codigo; }
-    public String getNombre() { return nombre; }
-    public String getDescripcion() { return descripcion; }
-    public double getPeso() { return peso; }
+    // getters
+    public int getCodigo() { return codigo; }
+    public String getNombre() { return nombre; }
+    public String getDescripcion() { return descripcion; }
+    public double getPeso() { return peso; }
 
-    // setters
-    public void setCodigo(int codigo) { this.codigo = codigo; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
-    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
-    public void setPeso(double peso) { this.peso = peso; }
+    // setters
+    public void setCodigo(int codigo) { this.codigo = codigo; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
+    public void setPeso(double peso) { this.peso = peso; }
 }
